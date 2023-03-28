56d2b751d1dd17881c8f7da1946b13cd1c955791
John caleb <Calebjohn3112@gmail.com>
2023-03-28 12:29:06 +0100

    Updated JS
    JS

--- js/main.js
+++ js/main.js
@@ -1,46 +1,39 @@
 //written by (https://github.com/johnocaleb), API by (https://themoviedb.org)
 var currentYear = new Date().getFullYear();
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const TOP_API_URL = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const API_KEY = "?api_key=3fd2be6f0c70a2a598f084ddfb75487c";
-//brake the single search url into two
-//first
-const SINGLE_API_URL_FIRST = 'https://api.themoviedb.org/3/movie/';
-//second
-const SINGLE_API_URL_SECOND = '?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US';
-const YEAR_API_URL = 'https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year='+currentYear+'&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
-const searchbtn = document.querySelector("#searchbtn");
-const searchlay = document.querySelector("#search-overlay");
-const singlelay = document.querySelector("#single-overlay");
-const searchBtnMobile = document.querySelector("#searchBtnMobile");
-const closeSearchBtn = document.querySelector("#closeSearchBtn");
-const closeSingleMovie = document.querySelector("#closeSingleMovie");
-const mobilemenu = document.querySelector("#mobile-menu");
-const moviecontainer = document.querySelector("#movie-container");
-const mobileoverlay = document.querySelector("#mobileoverlay");
-const searchresultheader = document.querySelector("#searchresultheader");
-const main = document.getElementById('main');
-const body = document.querySelector('body');
-const topMovieCurrentYearCover = document.querySelector('#topMovieCurrentYearCover');
-const popularMovieCover = document.querySelector('#popularMovieCover');
-const topMovieCover = document.querySelector('#topMovieCover');
-const search = document.getElementById('search');
-const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
-
-//event listener
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1',
+     TOP_API_URL = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1',
+     API_KEY = "?api_key=3fd2be6f0c70a2a598f084ddfb75487c",
+     SINGLE_API_URL_FIRST = 'https://api.themoviedb.org/3/movie/',
+     SINGLE_API_URL_SECOND = '?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US',
+     YEAR_API_URL = 'https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year='+currentYear+'&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1',
+     IMG_PATH = 'https://image.tmdb.org/t/p/w1280',
+     SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="',
+     searchbtn = document.querySelector("#searchbtn"),
+     searchlay = document.querySelector("#search-overlay"),
+     singlelay = document.querySelector("#single-overlay"),
+     searchBtnMobile = document.querySelector("#searchBtnMobile"),
+     closeSearchBtn = document.querySelector("#closeSearchBtn"),
+     closeSingleMovie = document.querySelector("#closeSingleMovie"),
+     mobilemenu = document.querySelector("#mobile-menu"),
+     moviecontainer = document.querySelector("#movie-container"),
+     mobileoverlay = document.querySelector("#mobileoverlay"),
+     searchresultheader = document.querySelector("#searchresultheader"),
+     main = document.getElementById('main'),
+     body = document.querySelector('body'),
+     topMovieCurrentYearCover = document.querySelector('#topMovieCurrentYearCover'),
+     popularMovieCover = document.querySelector('#popularMovieCover'),
+     topMovieCover = document.querySelector('#topMovieCover'),
+     search = document.getElementById('search'),
+     closeMobileOverlay = document.querySelector("#closeMobileOverlay");
     searchbtn.addEventListener("click", toogleoverlay);
     searchBtnMobile.addEventListener("click", toogleoverlay);
     closeSearchBtn.addEventListener("click", toogleoverlay);
     closeSingleMovie.addEventListener("click", toogleSingleView);
     mobilemenu.addEventListener("click", tooglemobilemenu);
     mobileoverlay.addEventListener("click", tooglemobilemenu);
-
     getPopularMovies(API_URL);
     getTopMovies(TOP_API_URL);
     getTopMoviesOfCurrentYear(YEAR_API_URL);
-    //for search bar 
     search.addEventListener("input",()=>{
         const searchTerm = search.value
         if(searchTerm && searchTerm !== '') {
@@ -51,7 +44,6 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             searchresultheader.textContent = "";
         }
     })
-
     function toogleoverlay() {
         if (searchlay.classList.contains("hidden")) {
             searchlay.classList.remove("hidden");
@@ -63,7 +55,6 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             body.classList.remove("overflow-none");
         }
     }
-
     function toogleSingleView() {
         if (singlelay.classList.contains("hidden")) {
             singlelay.classList.remove("hidden");
@@ -73,9 +64,9 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             singlelay.classList.remove("block");
             singlelay.classList.add("hidden");
             body.classList.remove("overflow-none");
+            document.querySelector("#singleMovieGenres").innerHTML = " ";
         }
     }
-
     function tooglemobilemenu() {
         if (mobileoverlay.classList.contains("hidden")) {
             mobileoverlay.classList.remove("hidden");
@@ -85,13 +76,11 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             mobileoverlay.classList.add("hidden");
         }
     }
-
     async function getMovies(url) {
         const res = await fetch(url)
         const data = await res.json()
         showMovies(data.results)
     }
-
     function showMovies(movies) {
         moviecontainer.innerHTML = ''
         searchresultheader.textContent = "Search Results:";
@@ -111,13 +100,11 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             moviecontainer.appendChild(movieEl)
         })
     }
-
     async function getPopularMovies(url) {
         const res = await fetch(url)
         const data = await res.json()
         showPopularMovies(data.results)
     }
-    //for popular movies
     function showPopularMovies(movies) {
         popularMovieCover.innerHTML = ''
         movies.forEach((movie) => {
@@ -136,17 +123,11 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             popularMovieCover.appendChild(movieEl)
         })
     }
-{/* <div class="overview">
-        <button onclick="getSingleMovie(`+id+`)" class="text-rose-400">View</button>
-            <h3>Overview</h3>
-            ${overview}
-            </div> */}
     async function getTopMovies(url) {
         const res = await fetch(url)
         const data = await res.json()
         showTopMovies(data.results)
     }
-//for top movies
     function showTopMovies(movies) {
         topMovieCover.innerHTML = ''
         movies.forEach((movie) => {
@@ -167,21 +148,17 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             topMovieCover.appendChild(movieEl)
         })
     }
-
     async function getTopMoviesOfCurrentYear(url) {
         const res = await fetch(url)
         const data = await res.json()
         showTopMoviesOfCurrentYear(data.results)
     }
-//for trending movies
     function showTopMoviesOfCurrentYear(movies) {
         topMovieCurrentYearCover.innerHTML = ''
         movies.forEach((movie) => {
             const { title, id, poster_path, vote_average, overview } = movie
-
             const movieEl = document.createElement('div')
             movieEl.classList.add('movie')
-            
             movieEl.innerHTML = `
             <div onclick="getSingleMovie(`+id+`)">
                 <img src="${IMG_PATH + poster_path}" alt="${title}">
@@ -194,28 +171,31 @@ const closeMobileOverlay = document.querySelector("#closeMobileOverlay");
             topMovieCurrentYearCover.appendChild(movieEl)
         })
     }
-
     async function getSingleMovie(url) {
         const res = await fetch(SINGLE_API_URL_FIRST+url+API_KEY)
         const data = await res.json()
         showSingleMovie(data);
     }
     function showSingleMovie(movies) {
+        movies.genres.forEach((genre) => {
+            const movieGenres = document.createElement('span')
+            movieGenres.textContent = genre.name;
+            document.querySelector("#singleMovieGenres").appendChild(movieGenres)
+        })
         document.querySelector("#movieOverview").textContent = movies.original_title.toUpperCase();
-        //movies.forEach((genres) => {})
         document.querySelector("#singleMovieFullOverview").textContent = movies.overview;
         document.querySelector("#singleMovieTagline").textContent = movies.tagline.toUpperCase();
         document.querySelector("#singleMovieWebsite").href = movies.homepage;
         document.querySelector("#singleMovieReleaseDate").textContent = movies.release_date;
         document.querySelector("#singleMovieBudget").textContent = movies.budget;
-        document.querySelector("#singleMovieRating").textContent = Math.round(movies.vote_average*10/10);
+        document.querySelector("#singleMovieRating").textContent = Math.round(movies.vote_average);
+        document.querySelector("#singleMovieRating").className = "";
         document.querySelector("#singleMovieRating").classList.add(getClassByRate(movies.vote_average));
         document.querySelector("#singleMovieStatus").textContent = movies.status.toUpperCase();
         document.querySelector("#singleMovieOverview").style.backgroundImage = "url("+IMG_PATH + movies.backdrop_path+")";
         document.querySelector("#singleMoviePoster").style.backgroundImage = "url("+IMG_PATH + movies.poster_path+")";
         toogleSingleView()
     }
-// to switch colors of votes
     function getClassByRate(vote) {
         if(vote >= 8) {
             return 'gold'
